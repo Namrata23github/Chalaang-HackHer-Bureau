502279655aa7078dd171af39b064ebb0d3077ad9
Namrata Agarwal <nagarwal@opentable.com>
2024-04-05 15:32:26 +0530

    Backend user

---
+++ Backend/run_model.py
@@ -0,0 +1,12 @@
+import joblib
+# Load the model
+# model = joblib.load('model.pkl')
+
+def make_prediction():
+    # Prepare the data
+   # data = input_data
+
+    # Make a prediction
+   # prediction = model.predict([input_data.array(list(data.values()))])
+    prediction = 0
+    return prediction

84c50484d12db1cd9b54d07f00d734233f3d85ab
amu18 <amritakumari654@gmail.com>
2024-04-05 18:19:59 +0530

    Basic structure and Header footer added

--- bureau-client/src/app/app.routes.ts
+++ bureau-client/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { RegistrationLoginFormComponent } from './components/registration-login-form/registration-login-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {path: '**', component: RegistrationLoginFormComponent},
+];
---
+++ bureau-client/src/app/components/registration-login-form/registration-login-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registration-login-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './registration-login-form.component.html',
+  styleUrl: './registration-login-form.component.scss'
+})
+export class RegistrationLoginFormComponent {
+
+}
